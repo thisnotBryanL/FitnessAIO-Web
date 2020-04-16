71b0266a638a33da7bdfcfdf5e0ca7d513d230b9
Bryan Lee <bryan_lee@baylor.edu>
2020-04-15 19:33:08 -0500

    Added pagination feature for food items to profile page

--- src/pages/filters.py
+++ src/pages/filters.py
@@ -4,9 +4,9 @@ from django_filters import DateFilter, CharFilter
 from foodEntry.models import FoodItem
 
 class FoodItemFilter(django_filters.FilterSet):
+    name = CharFilter(field_name='name', lookup_expr='icontains')
     start_date = DateFilter(field_name = 'date_added', lookup_expr = 'gte')
     end_date = DateFilter(field_name='date_added', lookup_expr='lte')
-    name = CharFilter(field_name='name', lookup_expr='icontains')
     class Meta:
         model = FoodItem
         fields = ['time']
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from calculator.calcFuncs import macroCalc, calcRemainCalories
 from foodEntry.models import FoodItem
@@ -17,6 +18,8 @@ def profilePage_view(request):
     # Query of all the food items associated with current user with dates in descending order
     foodlist = FoodItem.objects.filter(user=request.user).order_by('-date_added')
 
+
+
     numberOfEntries = len(foodlist)
 
     # Queryset of food items is run through the filter to get all food item parameters
@@ -25,11 +28,14 @@ def profilePage_view(request):
     # queryset is now filtered down with the search parmaeters
     foodlist = foodFilter.qs
 
-    
+    paginator = Paginator(foodlist, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     return render(request, 'profile.html',{
         'userProfile' : userProfile,
         'foodlist' : foodlist,
         'foodFilter' : foodFilter,
         'number_of_food': numberOfEntries,
+        'page_obj' : page_obj,
     })
