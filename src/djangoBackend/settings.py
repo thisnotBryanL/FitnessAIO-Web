43529f3b3fffe1f812d2f2acde39d6dcc844dbd1
Bryan Lee <bryan_lee@baylor.edu>
2020-04-19 19:46:55 -0500

    Database fixes pt2'

--- src/djangoBackend/settings.py
+++ src/djangoBackend/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'djangoBackend.wsgi.application'
 
 DATABASES={
     'default':{
-        'ENGINE': 'django.db.backends.sqlite3'
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
